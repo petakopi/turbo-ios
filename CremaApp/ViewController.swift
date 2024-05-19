1cdfd78232d2b7a9199fc82bc826aa27a28e9e37
Amree Zaid <mohd.amree@gmail.com>
2024-05-19 08:40:04 +0800

    Initial commit
    Use .gitignore from gitignore.io

---
+++ CremaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CremaApp
+//
+//  Created by Amree Zaid on 19/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
